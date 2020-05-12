2814bd52783180653044d974fa8c2197d7c71562
Juande Santander-Vela <juande.santander-vela@industriasinteligentes.cl>
2020-05-12 12:29:08 +0100

    Get date ranges

--- measure_calendar_occupancy.py
+++ measure_calendar_occupancy.py
@@ -1,5 +1,6 @@
 import os
 from exchangelib import Credentials, Configuration, Account, DELEGATE, EWSDateTime
+import datetime as dt
 
 email  = os.environ['EWS_EMAIL']
 user   = os.environ['EWS_USER']
@@ -11,4 +12,12 @@ config = Configuration(server=server, credentials=creds)
 account = Account(
     primary_smtp_address=email, config=config, autodiscover=False, access_type=DELEGATE
 )
+
+
+# Getting data for each week
+year=2020
+num_weeks = 25
+
+year_start = dt.datetime(year,1,1)
+delta = dt.timedelta(days=7)
 
